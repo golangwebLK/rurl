2dc67b1a4cc7de2d080fb75048c5471e995ae52d
liukang <LK13071172852@163.com>
2024-03-29 18:21:47 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+use clap::Parser;
+use serde_json::{Value};
+use reqwest::{Method};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(index = 1, required = true)]
+    url: String,
+
+    #[arg(short = 'X', long = "method")]
+    method: Method,
+
+    #[arg(short = 'H', long = "header")]
+    header: Option<Vec<String>>,
+
+    #[arg(short = 'd', long = "data")]
+    data: Option<String>,
+}
+
+
+#[tokio::main]
+async fn main(){
+    let args = Args::parse();
+    run(args.url, args.method, args.header, args.data).await;
+}
+
+async fn run(
+    url: String,
+    method: Method,
+    header: Option<Vec<String>>,
+    data: Option<String>
+){
+    let client = reqwest::Client::new();
+    let mut request_builder = client.request(method, url);
+
+    if let Some(header) = header {
+        request_builder = header.iter().fold(request_builder, |builder, item| {
+            let parts: Vec<&str> = item.split(':').collect();
+            if parts.len() == 2 {
+                builder.header(parts[0].trim().to_string(), parts[1].trim().to_string())
+            } else {
+                panic!("header格式错误: {}", item);
+            }
+        });
+    }
+
+
+    if let Some(data) = data{
+        let json_data:HashMap<String,Value> = serde_json::from_str(&data).expect("data数据格式错误");
+        request_builder = request_builder.json(&json_data);
+    }
+    let mut res = request_builder.send().await.expect("请求错误");
+    println!("status: {:#?}",res.status());
+    println!("headers: {:#?}",res.headers());
+    println!("content_length: {:#?}",res.content_length().expect("文本长度获取失败"));
+    println!("remote_addr: {:#?}",res.remote_addr().expect("远程地址获取失败"));
+    println!("body:");
+    while let Some(chunk) = res.chunk().await.expect("响应失败") {
+        if let Ok(utf8_string) = String::from_utf8(Vec::from(chunk.clone())) {
+            println!("{:#?}", utf8_string);
+        } else {
+            println!("{:#?}", String::from_utf8_lossy(&chunk));
+        }
+    }
+}
